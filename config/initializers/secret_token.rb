fa0e4e5986f8529985b757a2e1cea5b5b3613414
Krishnaswamy <subramk@thoughtworks.com>
2010-11-03 10:59:30 +0530

    Rail 3.0 scaffold for campus app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geekweek::Application.config.secret_token = '47ff6b662eb541e0d9f17fbf402719ce00b87b480670dab116d595af2f4a70f70cf14396ede89e66d764c1fcba953c8b36c2b4bffd7557529b7b5a786630ca8f'
